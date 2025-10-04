ec2aa0d7fa5c872049bdd1e415a8774300db717c
TetiankaSh <shh.tetiana@gmail.com>
2025-10-04 11:27:40 +0300

    initial commit with all the tests passed

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  itemWidth: number;
+  frameSize: number;
+  step: number;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +23,68 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+  };
+
+  handleItemWidthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ itemWidth: Number(e.target.value) });
+  };
+
+  handleFrameSizeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ frameSize: Number(e.target.value) });
+  };
+
+  handleStepChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ step: Number(e.target.value) });
   };
 
   render() {
-    const { images } = this.state;
+    const { images, itemWidth, frameSize, step } = this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <div>
+          <label htmlFor="itemId">Item Width (px):</label>
+          <input
+            id="itemId"
+            type="number"
+            value={itemWidth}
+            onChange={this.handleItemWidthChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="frameId">Frame Size:</label>
+          <input
+            id="frameId"
+            type="number"
+            value={frameSize}
+            onChange={this.handleFrameSizeChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="stepId">Step:</label>
+          <input
+            id="stepId"
+            type="number"
+            value={step}
+            onChange={this.handleStepChange}
+          />
+        </div>
+
+        <Carousel
+          images={images}
+          itemWidth={itemWidth}
+          frameSize={frameSize}
+          step={step}
+          animationDuration={1000}
+          infinite={false}
+        />
       </div>
     );
   }
